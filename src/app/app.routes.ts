c5bf37f994554bb62c1c8c238afe1f67ed93ccd1
Sébastien Burckhardt <sebastien.burckhardt@gmail.com>
2024-12-29 13:35:27 +0100

    maj

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
-import { Routes } from '@angular/router';
-import {HomeComponent} from "./features/home/components/home/home.component";
-import {ViewCoachesComponent} from "./features/coachs/components/coach-tabs/view-coaches/view-coaches.component";
-import {FormCoachComponent} from "./features/coachs/components/coach-tabs/form-coach/form-coach.component";
+import {Routes} from '@angular/router';
 import {CoachTabsComponent} from "./features/coachs/components/coach-tabs/coach-tabs.component";
 import {HallTabsComponent} from "./features/halls/components/hall-tabs/hall-tabs.component";
 import {TeamTabsComponent} from "./features/teams/components/team-tabs/team-tabs.component";
@@ -11,7 +8,7 @@ import {
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent},
+  { path: 'home', component: TeamTabsComponent},
   { path: 'coaches', component: CoachTabsComponent},
   { path: 'halls', component: HallTabsComponent},
   { path: 'teams', component: TeamTabsComponent},
--- src/app/core/interceptors/api-key.interceptor.ts
+++ src/app/core/interceptors/api-key.interceptor.ts
@@ -1,5 +1,7 @@
+
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from '@environments/environment';
+import {environment} from "@environments/environment";
+
 
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
   const keyValue = environment.apiKey.value;
