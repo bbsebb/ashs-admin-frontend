45943fe55639b2447c117efa66de3868c77bee27
bbseb <sebastien.burckhardt@gmail.com>
2025-02-21 22:04:14 +0100

    **Replace Google Maps with Mapbox Integration**
    Replaced Google Maps API with Mapbox implementation for improved flexibility and functionality. Updated relevant templates, components, and packages to reflect the transition, ensuring compatibility and adopting Mapbox's library dependencies.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, inject} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, inject} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -14,14 +14,18 @@ import {TeamService} from "./share/services/team.service";
 import {TRAINING_SESSION_SERVICE} from "./share/services/i-training-session.service";
 import {TrainingSessionService} from "./share/services/training-session.service";
 
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([apiKeyInterceptor])),
     {provide: COACH_SERVICE, useFactory: () => inject(CoachService)},
-    {provide: HALL_SERVICE, useFactory: () => inject(HallService)},
+    {
+      provide: HALL_SERVICE, useFactory: () =>  inject(HallService),
+    },
     {provide: TEAM_SERVICE, useFactory: () => inject(TeamService)},
-    {provide: TRAINING_SESSION_SERVICE, useFactory: () => inject(TrainingSessionService)}
+    {provide: TRAINING_SESSION_SERVICE, useFactory: () => inject(TrainingSessionService)},
   ]
 };
